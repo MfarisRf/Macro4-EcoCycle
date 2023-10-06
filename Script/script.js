31fc1af9f614218fa53cdbbacfe6e0f10460ff8d
MUHAMAD FARIS RAFI <89460895+MfarisRf@users.noreply.github.com>
2023-10-06 21:21:33 +0700

    Update(faris)

---
+++ Script/script.js
@@ -0,0 +1,23 @@
+// let z=2
+// switch (z) {
+//     case 1 :
+//         console.log("ini satu")
+//         break;
+//     case 2 :
+//         console.log("ini dua")
+//         break;
+//     default:
+//         console.log("ini gatau");
+
+//     throw error;
+//     }
+
+// for (let a = 0; a < 11;a--) {
+//     console.log(a);
+// }
+
+let i = 1;
+while (1 <= 100) {
+    console.log(i);
+    i++;
+}
